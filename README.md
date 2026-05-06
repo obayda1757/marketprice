baa55981441684ee096fdd6b0deb7d00d2242683
obayda1757 <obayda1757@yahoo.com>
2026-05-06 18:24:55 +0600

    Updated vite.config.ts

--- README.md
+++
@@ -1,3 +0,0 @@
-# marketprices
-Please visit https://www.bddata.org/prices/ for details.
-Developed by Amir Md Abu Obida
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this line below. It must match your GitHub repository name.
-  base: '/marketprice/', 
-  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
