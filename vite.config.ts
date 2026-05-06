3467db565d060eef084bc61b4c1739430afa1ce0
obayda1757 <obayda1757@yahoo.com>
2026-05-06 18:16:07 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add this line below. It must match your GitHub repository name.
+  base: '/marketprice/', 
+  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
